0ebeb55a539654bb19e0fa84269d6444097034bf
Zehra Aydoğdu <zehraaydogdu74@gmail.com>
2026-02-01 10:00:25 +0300

    Fibonacci serisini hesaplayan program eklendi

---
+++ Dongusel_Islemler/Fibonacci.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int a=0, b=1, c, i, n;
+	printf("Lutfen kac terim hesaplanacagini girin: ");
+	scanf("%d", &n);
+	printf("%d %d", a, b);
+	for(i=1; i<=n-2; i++){
+		c=a+b;
+		printf(" %d", c);
+		a=b;
+		b=c;
+	}
+	return 0;
+}
