35cd7ac3e814598646c2363c0984b9eca2ee201b
Zehra Aydoğdu <zehraaydogdu74@gmail.com>
2026-02-02 01:39:31 +0300

    Yıldızlarla tam piramit oluşturan program eklendi

---
+++ Dongusel_Islemler/Yildiz_Piramit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	int i, j, n, k;
+	printf("Kaca kaclik bir piramit istiyorsunuz: ");
+	scanf("%d", &n);
+	for(i=1; i<=n; i++){
+		for(j=1; j<=n-i; j++){
+			printf(" ");
+		}
+		for(k=1; k<=i; k++) {
+            printf("* ");
+        }
+		printf("\n");
+	}
+	return 0;
+}
